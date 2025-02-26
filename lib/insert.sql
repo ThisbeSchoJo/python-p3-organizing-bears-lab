d84f55779e1a987464f37fd5f02044dcb651d573
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-02-26 14:55:18 -0500

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 8, "M", "brown", "friendly", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 8, "M", "brown", "friendly", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 8, "M", "brown", "friendly", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 8, "M", "brown", "friendly", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 8, "M", "brown", "friendly", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 8, "M", "brown", "friendly", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 8, "M", "brown", "friendly", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 8, "M", "brown", "friendly", True);
